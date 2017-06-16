8b3d52ebc7ca4bc82fe4411633a93763e9983bad
Roy Lu <royvbtw@gmail.com>
2017-06-17 01:27:42 +0800

    Fix bunyan logger error
    Add a script in after-install.sh to fix error
    caused by bunyan logger.
    
    This script will create(mkdir) a 'log' folder after-install
    in the deploy stage. With this folder, the bunyan will work
    fine as expected.

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -6,4 +6,5 @@ sudo chown -R ec2-user /var/node/grumbler
 sudo chgrp -R ec2-user /var/node/grumbler
 chmod -R 755 /var/node/grumbler
 cd /var/node/grumbler
+mkdir log
 pm2 start /var/node/grumbler/index.js --name='grumbler'
